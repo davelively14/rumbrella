3f37bd33acc8f40aa23f088e3f6abfdf72117d63
Dave Lively <davelively14@gmail.com>
2016-03-28 12:50:15 -0400

    Updated backend test for InfoSys

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,8 +1,29 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # Stub module will act like our Wolfram backend, returning a response in the
+  # format that we expect. NOTE: A stub replaces real world libraries with
+  # simpler, predictable behavior. 
+  defmodule TestBackend do
+    def start_link(query, ref, owner, limit) do
+      Task.start_link(__MODULE__, :fetch, [query, ref, owner, limit])
+    end
+    def fetch("result", ref, owner, _limit) do
+      send(owner, {:results, ref, [%Result{backend: "test", text: "result"}]})
+    end
+    def fetch("none", ref, owner, _limit) do
+      send(owner, {:results, ref, []})
+    end
+  end
+
+  # Tests compute/2 functionality given the fake TestBackend module.
+  test "compute/2 with backend results" do
+    assert [%Result{backend: "test", text: "result"}] =
+           InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
   end
 end
